3451c18b980189bd0c9a49d6f5ae630532829a88
Dennis <dennimus@live.nl>
2019-01-31 14:43:28 +0100

    unit test les 1

--- CoolPackageName/cool_file_name.py
+++ CoolPackageName/cool_file_name.py
@@ -8,12 +8,27 @@ __license__ = "MIT"
 
 
 def main():
-    """ Main entry point of the app """
-    print(get_string())
+    """ 
+    Main entry point of the app 
+    """
+    x = input('First input: ')
+    y = input('Second input: ')
+    try:
+        result = add_inputs(x,y)
+        print(result)
+    except TypeError:
+        print('You have entered a string!')
+        main()
 
 
-def get_string():
-    return 'Hello world!'
+def add_inputs(first,second):
+    try: 
+        first = int(first) 
+        second = int(second)
+        return int(first) + int(second)
+    except ValueError:
+        raise TypeError
+
 
 
 if __name__ == "__main__":
--- CoolPackageName/tests/test_cool_file_name.py
+++ CoolPackageName/tests/test_cool_file_name.py
@@ -1,13 +1,67 @@
-"""
-You can auto-discover and run all tests with this command:
-
-    py.test
-
-Documentation: https://docs.pytest.org/en/latest/
-"""
-
 from CoolPackageName import cool_file_name
+import pytest
 
 
-def test_it_get_correct_string():
-    assert(cool_file_name.get_string() == 'Hello world!')
+def test_it_should_get_correct_answer_with_two_ints():
+    first = 10
+    second = 10
+    result = 20
+    test_result = cool_file_name.add_inputs(first,second)
+
+    assert(test_result == result)
+
+
+def test_it_should_raise_error_on_first_string_argument():
+    first = 'Jamemr joh'
+    second = 10
+    
+    with pytest.raises(TypeError):
+        cool_file_name.add_inputs(first,second)
+
+def test_it_should_raise_error_on_second_string_argument():
+    first = 10
+    second = 'Jamemr joh'
+    
+    with pytest.raises(TypeError):
+        cool_file_name.add_inputs(first,second)
+
+def test_it_should_work_with_negative_input():
+    first = -10
+    second = 5
+    result = -5
+    test_result = cool_file_name.add_inputs(first,second)
+
+    assert(test_result == result)
+
+
+def test_it_should_work_with_zero_as_input():
+    first = 0
+    second = 5
+    result = 5
+    test_result = cool_file_name.add_inputs(first,second)
+
+    assert(test_result == result)  
+
+
+def test_it_should_raise_error_on_empty_argument():
+    first = ''
+    second = 10
+    
+    with pytest.raises(TypeError):
+        cool_file_name.add_inputs(first,second)
+    
+def test_it_should_raise_error_on_None_argument():
+    first = None
+    second = 10
+    
+    with pytest.raises(TypeError):
+        cool_file_name.add_inputs(first,second)
+
+
+def test_it_should_work_with_maxint_as_input():
+    first = 5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+    second = 1
+    result = 5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555556
+    test_result = cool_file_name.add_inputs(first,second)
+
+    assert(test_result == result)  
